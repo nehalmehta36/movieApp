cd2c65befab717c8e32ee1104ed1b8be4974fceb
nehalmehta <nehal.mehta@myrealdata.net>
2022-09-20 14:43:07 +0530

    Initial Commit

---
+++ movieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieApp
+//
+//  Created by Nehal Mehta on 20/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
